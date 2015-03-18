ed22a229e184176cec99343970807ac408e5d958
Jane Lundgren <spilledmilk.gif@gmail.com>
2015-03-18 13:46:14 -0700

    set up layout

--- jottly-project/js/main.js
+++ jottly-project/js/main.js
@@ -4,3 +4,10 @@ $('#header').transify({
 	// fadeSpeed:1000,
 	percentWidth:'100%'
 });
+
+$('#navigation').transify({
+	opacityOrig:.8,
+	opacityNew:.8,
+	// fadeSpeed:1000,
+	percentWidth:'100%'
+});
