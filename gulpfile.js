c2f202121332d18e90b4a7f0cfeb7e50c608dca4
Jane Lundgren <jglundgren14@gmail.com>
2016-09-11 23:17:31 -0700

    --amend

--- gulpfile.js
+++ gulpfile.js
@@ -102,7 +102,7 @@ var site = require('./site.json');
   //---- DEPLOY ----//
   gulp.task('deploy', function() {
     return gulp.src('dist/**/*')
-               .pipe(plugins.ghPages(settings.ghPages));
+               .pipe(plugins.ghPages());
   });
 
   //---- WATCH ----//
